27a49c7a397f225552a1bc722cfd3b7ab9925ffb
Robert van Steen <studio@rovansteen.nl>
2016-04-07 18:25:06 +0200

    Store the received data in a database

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+var assert = require('assert');
+var Datastore = require('nedb');
 var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
+// Set up the server.
 var app = express();
+
+// Set up the database.
+var db = new Datastore({ filename: 'data.db', autoload: true });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
   console.log(req.body);
+
+  var device = req.body.device;
+  var value = req.body.value;
+  var sensor = req.body.sensor;
+  var date = Math.floor(Date.now() / 1000);
+
+  db.insert({
+    sensor: sensor,
+    value: value,
+    date: date,
+    device: device,
+  });
+
   res.send('OK');
 });
 
